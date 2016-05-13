388bdb5cda6b78503c95f5b72f78fc06bf32a00b
stiofan <info@wpgeodirectory.com>
2016-05-13 20:56:05 +0100

    temp2

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php get_header(); ?>
+    <div class="featured-area">
+        <div class="featured-img" <?php
+        $page_for_posts = get_option('page_for_posts');
+        if (has_post_thumbnail($page_for_posts)) { // check if the post has a Post Thumbnail assigned to it.
+            $full_image_url = wp_get_attachment_image_src(get_post_thumbnail_id($page_for_posts), 'full');
+        }else{
+            $full_image_url[0] = SD_DEFAULT_FEATURED_IMAGE;
+        }
+            ?> style="background-image: url(<?php echo $full_image_url[0]; ?>);" <?php
+        ?>>
+
+        </div>
+        <div class="header-wrap">
+
+            <h1 class="entry-title"><?php echo get_the_title($page_for_posts); ?></h1>
+            <?php if (get_post_meta($page_for_posts, 'subtitle', true)) {
+                echo '<div class="entry-subtitle">' . get_post_meta($page_for_posts, 'subtitle', true) . '</div>';
+            } ?>
+
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="content-box content-single">
+            <?php if (!have_posts()) : ?>
+                <div class="alert-error">
+                    <p><?php _e('Sorry, no results were found.', 'directory-starter'); ?></p>
+                </div>
+                <?php get_search_form(); ?>
+            <?php endif; ?>
+            <?php
+            while (have_posts()) : the_post();
+
+                // Include the page content template.
+                get_template_part('content-blog');
+
+                // End the loop.
+            endwhile;
+
+            // Previous/next page navigation.
+            the_posts_pagination(array(
+                'prev_text' => __('Previous', 'directory-starter'),
+                'next_text' => __('Next', 'directory-starter'),
+            ));
+            ?>
+        </div>
+    </div>
+<?php get_footer(); ?>
